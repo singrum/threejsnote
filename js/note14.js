46820c780af10f63e7117e041b6b06b80d8d1f95
Kang Hyomin <103807515+singrum@users.noreply.github.com>
2023-02-10 10:24:53 +0000

    x

--- js/note14.js
+++ js/note14.js
@@ -1,6 +1,10 @@
 import * as THREE from '../node_modules/three/build/three.module.js';
 import {OrbitControls} from '../node_modules/three/examples/jsm/controls/OrbitControls.js';
 
+// https://www.humus.name
+// https://polyhaven.com/a/sunflowers_puresky
+
+
 class App {
 	constructor() {
 		const divContainer = document.querySelector("#webgl_container");
@@ -16,8 +20,9 @@ class App {
 
 		this._setupCamera();
 		this._setupLight();
-		this._setupModel();
+		// this._setupModel();
 		this._setupControls()
+		this._setupBackground();
 
 		window.onresize = this.resize.bind(this);
 		this.resize();
@@ -32,8 +37,8 @@ class App {
 	_setupCamera() {
 		const width = this._divContainer.clientWidth;
 		const height = this._divContainer.clientHeight;
-		const camera = new THREE.PerspectiveCamera(75, width / height, 0.1, 100);
-		camera.position.z = 2;
+		const camera = new THREE.PerspectiveCamera(75, width / height, 0.1, 1000);
+		camera.position.z = 80;
 		this._camera = camera;
 	}
 
@@ -45,18 +50,62 @@ class App {
 		this._scene.add(light);
 	}
 
+	_setupBackground(){
+		// 색상 배경
+		// this._scene.background = new THREE.Color("#9b59b6")
+
+		// 이미지 배경
+		// const loader = new THREE.TextureLoader();
+		// loader.load("../data/uv_grid.jpg", texture => {
+		// 	this._scene.background = texture;
+		// 	this._setupModel();
+		// })
+
+		// 큐브맵
+		// const loader = new THREE.CubeTextureLoader();
+		// loader.load([
+		// 	"../data/cubemap/posx.jpg",
+		// 	"../data/cubemap/negx.jpg",
+		// 	"../data/cubemap/posy.jpg",
+		// 	"../data/cubemap/negy.jpg",
+		// 	"../data/cubemap/posz.jpg",
+		// 	"../data/cubemap/negz.jpg"
+		// ], cubeTexture => {
+		// 	this._scene.background = cubeTexture;
+		// 	this._setupModel();
+		// })
+
+		// 정방형 맵
+		const loader = new THREE.TextureLoader();
+		loader.load("../data/background.jpg", texture => {
+			const renderTarget = new THREE.WebGLCubeRenderTarget(texture.image.height);
+			renderTarget.fromEquirectangularTexture(this._renderer, texture);
+			this._scene.background = renderTarget.texture;
+			this._setupModel();
+		})
+		
+	}
+	
 	_setupModel() {
-		const geometry = new THREE.SphereBufferGeometry();
+		//배경이 비치게
+		const pmremG = new THREE.PMREMGenerator(this._renderer);
+		// const renderTarget = pmremG.fromCubemap(this._scene.background)
+		const renderTarget = pmremG.fromEquirectangular(this._scene.background)
+
+
+		const geometry = new THREE.SphereGeometry();
 
 		const material1 = new THREE.MeshStandardMaterial({
 			color: "#2ecc71",
-			roughness: 0.3,
-			metalness:0.9,
+			roughness: 0,
+			metalness: 1,
+			envMap: renderTarget.texture
 		})
 		const material2 = new THREE.MeshStandardMaterial({
 			color: "#e74c3c",
-			roughness: 0.3,
-			metalness: 0.9,
+			roughness: 0,
+			metalness: 1,
+			envMap: renderTarget.texture
 		})
 		const rangeMin = -20.0, rangeMax = 20.0;
 		const gap = 10.0;
