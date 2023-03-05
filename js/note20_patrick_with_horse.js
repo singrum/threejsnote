105385796e1de89c5931da4ac2ef843af55003ed
Kang Hyomin <103807515+singrum@users.noreply.github.com>
2023-03-05 14:00:14 +0000

    x

--- js/note20_patrick_with_horse.js
+++ js/note20_patrick_with_horse.js
@@ -18,74 +18,117 @@ class App {
 		const scene = new THREE.Scene();
 		this._scene = scene;
 
+
         this._setupBackground();
 		this._setupCamera();
 		this._setupLight();
 		this._setupModel();
-		this._setupControls()
+		
 
 		window.onresize = this.resize.bind(this);
 		this.resize();
 
-		requestAnimationFrame(this.render.bind(this));
+		
 	}
 
     _setupBackground(){
         this._scene.background = new THREE.Color(0xdddddd);
     }
 	_setupControls(){
-		new OrbitControls(this._camera, this._divContainer);
+
+		const onPointerDown = ( event ) => {
+			
+			if ( event.isPrimary === false ) return;
+
+			this.pointerXOnPointerDown = event.clientX - window.innerWidth / 2;
+			this.targetRotationOnPointerDown = this.targetRotation;
+
+			document.addEventListener( 'pointermove', onPointerMove );
+			document.addEventListener( 'pointerup', onPointerUp );
+
+		}
+
+		const onPointerMove = ( event ) => {
+			
+			if ( event.isPrimary === false ) return;
+			
+			this.pointerX = event.clientX - window.innerWidth / 2;
+
+			this.targetRotation = this.targetRotationOnPointerDown + ( this.pointerX - this.pointerXOnPointerDown ) * 0.02;
+			
+
+		}
+
+		const onPointerUp = (event) => {
+
+			if ( event.isPrimary === false ) return;
+
+			document.removeEventListener( 'pointermove', onPointerMove );
+			document.removeEventListener( 'pointerup', onPointerUp );
+
+		}
+		this.targetRotation = 0;
+		this.targetRotationOnPointerDown = 0;
+		this.pointerX = 0;
+		this.pointerXOnPointerDown = 0;
+		
+		this._divContainer.style.touchAction = 'none';
+		this._divContainer.addEventListener( 'pointerdown', onPointerDown );
+
+
 	}
 
-    _zoomFit(object3D, camera){
-        const box = new THREE.Box3().setFromObject(object3D);
-        const sizeBox = box.getSize(new THREE.Vector3()).length();
-        const centerBox = box.getCenter(new THREE.Vector3());
-        const halfSizeModel = sizeBox * 0.5;
-        const halfFov = THREE.MathUtils.degToRad(camera.fov * 0.5);
-
-        const distance = halfSizeModel / Math.tan(halfFov);
-        const direction = (new THREE.Vector3()).subVectors(
-            camera.position, centerBox).normalize();
-
-        const position = direction.multiplyScalar(distance).add(centerBox);
-        camera.position.copy(position);
-
-        camera.near = sizeBox / 100;
-        camera.far = sizeBox * 100;
-
-        camera.updateProjectionMatrix();
-        camera.lookAt(centerBox.x, centerBox.y, centerBox.z)
-
-    }
 	_setupCamera() {
 		const width = this._divContainer.clientWidth;
 		const height = this._divContainer.clientHeight;
 		const camera = new THREE.PerspectiveCamera(75, width / height, 0.1, 1000);
-		camera.position.z = 2;
+		camera.position.set(0,3,-10);
 		this._camera = camera;
         this._scene.add(this._camera)
+		this._camera.lookAt(0,3,0)
 	}
 
 	_setupLight() {
 		const color = 0xffffff;
 		const intensity = 1;
 		const light = new THREE.DirectionalLight(color, intensity);
-		light.position.set(-1, 2, 4);
-		this._camera.add(light);
+		light.position.set(0, 3, -10);
+		this._scene.add(light);
 	}
 
 	_setupModel() {
+		const axesHelper = new THREE.AxesHelper(5)
+		this._scene.add(axesHelper)
+
 		const gltfLoader = new GLTFLoader()
         const url = '../data/patrick/scene.gltf';
         gltfLoader.load(
             url,
             (gltf)=>{
-                const root = gltf.scene;
-                this._scene.add(root);
-                this._zoomFit(root, this._camera)
+                const root = gltf.scene.children[0];
+				this.group = root;
+                // this._scene.add(root);
+				const children = root.children[0].children;
+				
+				children.splice(2,3)
+				// 받침대 : children[0], children[1]
+				// 우편함 : children[2],children[3], children[4]
+				// 해마&뚱이 : children[5]
+				// console.log(root)
+				this._scene.add(root)
+				children[2].rotation.y = 1
+				// console.log(root.children[0].children[0].children)
+
+
+
+
+
+
+				this._setupControls()
+				requestAnimationFrame(this.render.bind(this));
             }
         )
+
 	}
 
 	resize() {
@@ -99,15 +142,21 @@ class App {
 	}
 
 	render(time) {
-		this._renderer.render(this._scene, this._camera);
+
 		this.update(time);
 		requestAnimationFrame(this.render.bind(this));
 	}
 
 	update(time) {
 		time *= 0.001;
-		// this._cube.rotation.x = time;
-		// this._cube.rotation.y = time;
+		this.group.rotation.z += ( this.targetRotation - this.group.rotation.z ) * 0.05;
+		
+
+		this._camera.lookAt(0,3,0);
+
+		this._renderer.clear();
+		this._renderer.render(this._scene, this._camera);
+
 	}
 }
 
