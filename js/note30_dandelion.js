23f464284cbf695f436c021f66420725dc17077d
Kang Hyomin <103807515+singrum@users.noreply.github.com>
2023-04-01 11:49:49 +0000

    x

--- js/note30_dandelion.js
+++ js/note30_dandelion.js
@@ -147,14 +147,14 @@ class App {
 		for(let i = 0 ; i< dandelionCoreGeom.positionVectors.length; i++){
 			const points = [];
 			points.push(dandelionCoreGeom.positionVectors[i], dandelionSkinGeom.positionVectors[i]);
-			
 			const geometry = new THREE.BufferGeometry().setFromPoints( points );
 			const line = new THREE.Line( geometry, new THREE.LineBasicMaterial({color : 0xffffff}) );
 			
-			lines.push(line);
+			this._scene.add(line)
+
+			
 		}
 
-		lines.forEach(e=>this._scene.add(e))
 
 
 	}
