1eba65fef40622cdb91bb928bc2505680a0add47
Kang Hyomin <103807515+singrum@users.noreply.github.com>
2023-04-28 14:50:55 +0000

    x

--- js/note32_rainbow.js
+++ js/note32_rainbow.js
@@ -183,7 +183,7 @@ class App {
 		cloudArr = cloudArr.concat(cloudArr.map(cloud =>cloud.clone()));
 		this.cloudArr = cloudArr;
 		for(let cloud of cloudArr){
-			cloud.position.set(randRange(-30,30),randRange(10,30),randRange(-10,10));
+			cloud.position.set(randRange(-30,30),randRange(10,40),randRange(-10,10));
 			cloud.posXinit = cloud.position.x
 			cloud.rand1 = randRange(0.5,1);
 			this._scene.add(cloud)
