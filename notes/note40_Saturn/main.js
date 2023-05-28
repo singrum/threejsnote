ebb2a64e44fc1323e0bf5e26f3587e420c876281
Kang Hyomin <103807515+singrum@users.noreply.github.com>
2023-05-28 04:15:01 +0000

    x

--- notes/note40_Saturn/main.js
+++ notes/note40_Saturn/main.js
@@ -93,7 +93,7 @@ class App {
 			if ( event.isPrimary === false ) return;
 			this.pointerX = event.clientX - window.innerWidth / 2;
 
-			this.targetRotation = this.targetRotationOnPointerDown + ( this.pointerX - this.pointerXOnPointerDown ) * 0.1;
+			this.targetRotation = this.targetRotationOnPointerDown + ( this.pointerX - this.pointerXOnPointerDown ) * 0.5;
 			
 
 		}
@@ -232,9 +232,9 @@ class App {
 		
 	}
 	update() {
-		this.angle += ( this.targetRotation - this.angle ) * 0.02;
+		this.angle += ( this.targetRotation - this.angle ) * 0.01;
 		this.sphere.rotation.y =this.angle;
-		this.torsion = Math.abs(this.targetRotation - this.angle) * 0.2;
+		this.torsion = Math.abs(this.targetRotation - this.angle) * 0.1;
 		// console.log(this.torsion)
 	}
 	
